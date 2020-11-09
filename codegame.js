60fcf5a7b7061a6771220eec2324949e67854d93
omerzilber1403 <73109183+omerzilber1403@users.noreply.github.com>
2020-11-09 17:21:10 +0200

    Add files via upload

--- codegame.js
+++ codegame.js
@@ -5,15 +5,16 @@ $(function () {
 });
 
 player = {
-  x: screen.width / 3.5,
-  y: screen.height /1.7,
-  width: screen.width/3,
-  height: screen.height/3,
+  x: screen.width / 3,
+  y: screen.height / 1.7,
+  width: screen.width / 3,
+  height: screen.height / 3,
   frameX: 0,
   frameY: 0,
-  speed: 9,
+  speed: screen.width / 3,
   moving: false,
 };
+
 // [left,right]
 moving = [false, false];
 
@@ -24,6 +25,16 @@ const playerSprite = new Image();
 playerSprite.src = "images/car_up.png";
 const background = new Image();
 background.src = "images/road.png";
+console.log(background);
+
+road1 = {
+  y: 0,
+  speed: screen.height / 5,
+};
+
+road2 = {
+  y: -screen.height,
+};
 
 function init() {
   canvas = document.getElementById("canvas");
@@ -38,7 +49,15 @@ function drawSprite(img, sX, Sy, sW, sH, dX, dY, dW, dH) {
 }
 
 function animate() {
-  ctx.drawImage(background, 0, 0, screen.width, screen.height);
+  ctx.clearRect (0, 0, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, road1.y, screen.width, screen.height);
+  ctx.drawImage(
+    background,
+    0,
+    road2.y,
+    screen.width,
+    screen.height
+  );
   drawSprite(
     playerSprite,
     0,
@@ -51,6 +70,7 @@ function animate() {
     player.height
   );
   movePlayer();
+  moveRoad();
   requestAnimationFrame(animate);
 }
 
@@ -108,12 +128,21 @@ function handleTouchMove(evt) {
 
 function movePlayer() {
   if (moving[0] && carPosition > 0) {
-    player.x -= screen.width/3;
+    player.x -= player.speed;
     moving[0] = false;
     carPosition -= 1;
   } else if (moving[1] && carPosition < 2) {
-    player.x += screen.width/3;
+    player.x += player.speed;
     moving[1] = false;
     carPosition += 1;
+  }
+}
+
+function moveRoad() {
+  road1.y += road1.speed;
+  road2.y += road1.speed;
+  if (road1.y === screen.height) {
+    road1.y = 0;
+    road2.y = -screen.height;
   }
 }
